5caa4cb48cc83b06f3695a3a0e525340e618ccd3
ItsCetrion <goodplay2.0@mail.ru>
2025-07-04 14:25:25 +0700

    changed structure of project
    added minio container for media data

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,53 +1,32 @@
 package main
 
 import (
-	"auth-service/internal/config/dbconfig"
+	"shared/config/dbconfig"
 	"auth-service/internal/controllers"
 	"auth-service/internal/grpc"
 
-	// "auth-service/internal/middleware"
 	"auth-service/internal/migrations"
 	"auth-service/internal/pkg/jwt"
 	"auth-service/internal/repository"
 	"auth-service/internal/router"
 	"auth-service/internal/services"
-	"fmt"
 	"log"
 	"net/http"
 	"shared/middleware"
-
-	"github.com/jmoiron/sqlx"
 )
 
 
 func main() {
-
-	//переделать инициализацию подключения к бд
 	dbconf := dbconfig.NewDBConfig()
-
-	conn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
-			dbconf.User,
-			dbconf.Password,
-			dbconf.Host,
-			dbconf.Port,
-			dbconf.Name,
-			dbconf.SSLMode)
-
+	conn := dbconf.CreateConnection()
 	err := migrations.RunMigration(conn, "internal/migrations")
 	if err != nil {
 		log.Fatalf("Migration error: %v", err)
 	}
-	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		dbconf.Host,
-		dbconf.Port,
-		dbconf.User,
-		dbconf.Name,
-		dbconf.Password,
-		dbconf.SSLMode))
+	db, err := dbconf.CreateDSN()
 	if err != nil {
 		log.Fatal("DB connection error: ", err)
 	}
-
 
 	userRepo := repository.NewUserRepository(db)
 	tokenRepo := repository.NewTokenRepository(db)
--- notes-service/cmd/main.go
+++ notes-service/cmd/main.go
@@ -1,47 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"notes-service/internal/config/dbconfig"
 	"notes-service/internal/controllers"
 	"notes-service/internal/migrations"
 	"notes-service/internal/repository"
 	"notes-service/internal/router"
 	"notes-service/internal/services"
 	"shared/middleware"
-	_ "shared/middleware"
-
-	"github.com/jmoiron/sqlx"
+	"shared/config/dbconfig"
 	"google.golang.org/grpc"
 )
 
 
 func main() {
-
-	//переделать инициализацию подключения к бд
 	dbconf := dbconfig.NewDBConfig()
-
-	conn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
-			dbconf.User,
-			dbconf.Password,
-			dbconf.Host,
-			dbconf.Port,
-			dbconf.Name,
-			dbconf.SSLMode)
-
+	conn := dbconf.CreateConnection()
 	err := migrations.RunMigration(conn, "internal/migrations")
 	if err != nil {
 		log.Fatalf("Migration error: %v", err)
 	}
-	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		dbconf.Host,
-		dbconf.Port,
-		dbconf.User,
-		dbconf.Name,
-		dbconf.Password,
-		dbconf.SSLMode))
+	db, err := dbconf.CreateDSN()
 	if err != nil {
 		log.Fatal("DB connection error: ", err)
 	}
