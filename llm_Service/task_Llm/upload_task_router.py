ce46dbecfef661932594ff67337cb5b1a181a4fb
TFed555 <144507244+TFed555@users.noreply.github.com>
2025-07-16 17:13:42 +0700

    Update upload_task_router.py

--- llm_Service/task_Llm/upload_task_router.py
+++ llm_Service/task_Llm/upload_task_router.py
@@ -15,7 +15,7 @@ uploadTask = APIRouter(prefix = "/api/task_llm",tags=["Отправка цели
 def  sendToChequeInfo(reqTaskLLM: ReqTaskLLM):
     urlHost = "https://localhost:8082"
     plan = taskMasterLLM.getPlan(task=reqTaskLLM.task,description=reqTaskLLM.description,todoId = reqTaskLLM.todoID)
-    url = urlHost + "/api/todos/plan"
+    url = "https://localhost:8082/api/todos/plan"
     requests.post(url=url, data=plan)
     return "Send successfully"
  
